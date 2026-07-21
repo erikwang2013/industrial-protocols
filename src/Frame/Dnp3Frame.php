18c0a1f7b6d3e119f98d511a5d8b757436a18afc
erik <erik@erik.xyz>
2026-07-22 01:23:31 +0800

    更新版权

--- src/Frame/Dnp3Frame.php
+++ src/Frame/Dnp3Frame.php
@@ -1,5 +1,9 @@
 <?php
 
+/*
+ * Copyright (c) 2026 erik <erik@erik.xyz> — https://erik.xyz
+ */
+
 namespace Erikwang2013\IndustrialProtocols\Dnp3\Frame;
 
 use Erikwang2013\IndustrialProtocols\Exception\FrameException;
