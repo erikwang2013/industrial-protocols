dd79ffb0b9283d27b5ff72c76a60fa305f553706
erik <erik@erik.xyz>
2026-07-22 01:02:19 +0800

    新增 5 个专用/IoT 协议包

---
+++ src/Exception/Iec61850Exception.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Erikwang2013\IndustrialProtocols\Iec61850\Exception;
+
+use Erikwang2013\IndustrialProtocols\Exception\ProtocolException;
+
+class Iec61850Exception extends ProtocolException {}
