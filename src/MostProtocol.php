5fb25f1ca74afabf5198531345dec6e61e76286b
erik <erik@erik.xyz>
2026-07-22 01:14:18 +0800

    新增11 个协议包

---
+++ src/MostProtocol.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Erikwang2013\IndustrialProtocols\Most;
+
+use Erikwang2013\IndustrialProtocols\Bridge\BridgeConnector;
+use Erikwang2013\IndustrialProtocols\Protocol\ConnectorInterface;
+use Erikwang2013\IndustrialProtocols\Protocol\ProtocolInterface;
+
+class MostProtocol implements ProtocolInterface
+{
+    public function getName(): string { return 'most'; }
+    public function getVersion(): string { return '1.0.0'; }
+    public function getSupportedVariants(): array { return ['bridge']; }
+    public function getDefaultPort(): int { return 0; }
+
+    public function createConnector(array $config): ConnectorInterface
+    {
+        if (!isset($config['bridge'])) {
+            throw new \RuntimeException("MOST requires a BridgeInterface in config['bridge']");
+        }
+        return new BridgeConnector($config['bridge'], 'most');
+    }
+}
