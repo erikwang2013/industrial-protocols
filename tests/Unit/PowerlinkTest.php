844cd6e7a73b1758653021dccbb875a6488d057e
erik <erik@erik.xyz>
2026-07-22 00:04:25 +0800

    feat: add hardware bridge layer and EtherCAT/POWERLINK/SERCOS III protocol stubs

---
+++ tests/Unit/PowerlinkTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Erikwang2013\IndustrialProtocols\Powerlink\Tests\Unit;
+
+use Erikwang2013\IndustrialProtocols\Bridge\TcpGatewayBridge;
+use Erikwang2013\IndustrialProtocols\Powerlink\PowerlinkProtocol;
+use PHPUnit\Framework\TestCase;
+
+class PowerlinkTest extends TestCase
+{
+    public function testProtocolMetadata(): void
+    {
+        $p = new PowerlinkProtocol();
+        $this->assertSame('powerlink', $p->getName());
+        $this->assertSame('1.0.0', $p->getVersion());
+        $this->assertContains('bridge', $p->getSupportedVariants());
+    }
+
+    public function testRequiresBridge(): void
+    {
+        $p = new PowerlinkProtocol();
+        $this->expectException(\RuntimeException::class);
+        $p->createConnector([]);
+    }
+
+    public function testCreateConnectorWithBridge(): void
+    {
+        $p = new PowerlinkProtocol();
+        $bridge = new TcpGatewayBridge('192.168.1.100', 5555);
+        $conn = $p->createConnector(['bridge' => $bridge]);
+        $this->assertFalse($conn->isConnected());
+    }
+}
