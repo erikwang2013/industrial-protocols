d70d9becffcc29311f2171f200b66dc06184bd89
erik <erik@erik.xyz>
2026-07-22 01:23:31 +0800

    更新版权

--- src/Driver/CcLinkDriver.php
+++ src/Driver/CcLinkDriver.php
@@ -1,5 +1,9 @@
 <?php
 
+/*
+ * Copyright (c) 2026 erik <erik@erik.xyz> — https://erik.xyz
+ */
+
 namespace Erikwang2013\IndustrialProtocols\CcLink\Driver;
 
 use Erikwang2013\IndustrialProtocols\CcLink\Exception\CcLinkException;
