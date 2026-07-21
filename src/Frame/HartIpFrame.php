9acd086b5d2ab98042b47db1c6fe3af048b69ba6
erik <erik@erik.xyz>
2026-07-22 01:23:31 +0800

    更新版权

--- src/Frame/HartIpFrame.php
+++ src/Frame/HartIpFrame.php
@@ -1,5 +1,9 @@
 <?php
 
+/*
+ * Copyright (c) 2026 erik <erik@erik.xyz> — https://erik.xyz
+ */
+
 namespace Erikwang2013\IndustrialProtocols\HartIp\Frame;
 
 use Erikwang2013\IndustrialProtocols\HartIp\Exception\HartIpException;
