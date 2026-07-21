a864ee86ad7edccb4bb3b55ffd31b7d939a63759
erik <erik@erik.xyz>
2026-07-22 01:14:18 +0800

    新增11 个协议包

---
+++ src/Exception/HartIpException.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Erikwang2013\IndustrialProtocols\HartIp\Exception;
+
+class HartIpException extends \RuntimeException
+{
+    public static function invalidVersion(int $version): self
+    {
+        return new self(sprintf('Invalid HART-IP protocol version: %d (expected 1)', $version));
+    }
+
+    public static function invalidMessageType(int $type): self
+    {
+        return new self(sprintf('Invalid HART-IP message type: %d', $type));
+    }
+
+    public static function headerTooShort(int $length): self
+    {
+        return new self(sprintf('HART-IP header too short: %d bytes (minimum 8)', $length));
+    }
+
+    public static function connectionFailed(string $address, int $port): self
+    {
+        return new self(sprintf('HART-IP connection failed: %s:%d', $address, $port));
+    }
+}
